50f3ec278a571e334925235910300284d48af33b
Renan <rmavansi@gmail.com>
2019-11-13 21:37:24 -0300

    loading and toasty auth

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,27 +1,34 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
 
+import { toast } from 'react-toastify';
+
 import history from '~/services/history';
 import api from '~/services/api';
 
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, 'sessions', {
-    email,
-    password,
-  });
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
 
-  const { token, user } = response.data;
-  if (!user.provider) {
-    console.tron.error('User is not a provider.');
-    return;
+    const { token, user } = response.data;
+    if (!user.provider) {
+      toast.error('User is not a provider');
+      return;
+    }
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (err) {
+    toast.error('Authentication failure');
+    yield put(signFailure());
   }
-
-  yield put(signInSuccess(token, user));
-
-  history.push('/dashboard');
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
