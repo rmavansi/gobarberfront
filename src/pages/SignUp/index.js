7d82ccbd06280fff47d1cbf09ff705cce3039d19
Renan <rmavansi@gmail.com>
2019-11-24 18:36:34 -0300

    feature: mark notification as read

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,23 +1,28 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
-export default function SignUp() {
-  const schema = Yup.object().shape({
-    name: Yup.string().required('Name is required'),
-    email: Yup.string()
-      .email('Insert a valid email')
-      .required('Email is required'),
-    password: Yup.string()
-      .min(6, 'At least 6 characters')
-      .required('Password is required'),
-  });
+import { signUpRequest } from '~/store/modules/auth/actions';
 
-  function handleSubmit(data) {
-    console.tron.log(data);
+const schema = Yup.object().shape({
+  name: Yup.string().required('Name is required'),
+  email: Yup.string()
+    .email('Insert a valid email')
+    .required('Email is required'),
+  password: Yup.string()
+    .min(6, 'At least 6 characters')
+    .required('Password is required'),
+});
+
+export default function SignUp() {
+  const dispatch = useDispatch();
+
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
   }
   return (
     <>
